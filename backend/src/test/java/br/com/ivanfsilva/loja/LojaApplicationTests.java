adbae6df49d0f6be627a9546522caf337acd2fdd
ivanfsilva <ivanfs27@gmail.com>
2021-11-12 23:45:37 -0300

    first commit

---
+++ backend/src/test/java/br/com/ivanfsilva/loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ivanfsilva.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
