c1d1afdae3e59bd6da562aa07123c45e7de2b7bc
ivanfsilva <ivanfs27@gmail.com>
2021-11-14 10:40:14 -0300

    adding the budget situation

--- backend/src/main/java/br/com/ivanfsilva/loja/model/Orcamento.java
+++ backend/src/main/java/br/com/ivanfsilva/loja/model/Orcamento.java
@@ -12,6 +12,18 @@ public class Orcamento {
         this.quantidadeItens = quantidadeItens;
     }
 
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+
+        if ( situacao.equals("EM_ANALISE") ) {
+            valorDoDescontoExtra = new BigDecimal("0.05");
+        } else if ( situacao.equals("APROVADO")) {
+            this.valor = this.valor.subtract(valorDoDescontoExtra);
+        }
+
+        this.valor = this.valor.subtract( valorDoDescontoExtra );
+    }
+
     public BigDecimal getValor() {
         return valor;
     }
