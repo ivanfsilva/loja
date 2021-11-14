e7b3f444e13383ead101aa953ffb515d12d6c2e3
ivanfsilva <ivanfs27@gmail.com>
2021-11-14 11:34:07 -0300

    generating an order

--- backend/src/main/java/br/com/ivanfsilva/loja/model/Finalizado.java
+++ backend/src/main/java/br/com/ivanfsilva/loja/model/Finalizado.java
@@ -1,7 +1,5 @@
 package br.com.ivanfsilva.loja.model;
 
-import java.math.BigDecimal;
-
 public class Finalizado extends SituacaoOrcamento {
 
 }
---
+++ backend/src/main/java/br/com/ivanfsilva/loja/pedido/Pedido.java
@@ -0,0 +1,32 @@
+package br.com.ivanfsilva.loja.pedido;
+
+import br.com.ivanfsilva.loja.model.Orcamento;
+
+import java.time.LocalDateTime;
+
+public class Pedido {
+
+    private String cliente;
+    private LocalDateTime data;
+    private Orcamento orcamento;
+
+    public Pedido(String cliente, LocalDateTime data, Orcamento orcamento) {
+        this.cliente = cliente;
+        this.data = data;
+        this.orcamento = orcamento;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public Orcamento getOrcamento() {
+        return orcamento;
+    }
+
+
+}
